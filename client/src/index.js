248605c28f80be237f5dd731894a1b884eab6cee
Daniel <81238732+danielaglr@users.noreply.github.com>
2023-02-11 14:54:24 -0800

    Added routes for different views

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,9 @@ import Landing from './views/landing';
 import SignUp from './views/signup';
 import LogIn from './views/login';
 import Home from './views/home';
+import Lifts from './views/lifts';
+import Data from './views/data';
+import Profile from './views/profile';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -19,6 +22,9 @@ root.render(
         <Route path='/signup' element={<SignUp />} />
         <Route path='/login' element={<LogIn />} />
         <Route path='/home' element={<Home />} />
+        <Route path='/lifts' element={<Lifts />} />
+        <Route path='/data' element={<Data />} />
+        <Route path='/profile' element={<Profile />} />
       </Routes>
     </AuthProvider>
   </BrowserRouter>
